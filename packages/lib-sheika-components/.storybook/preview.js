50b14c7d148e5afa08029c9d4fca236a8390c5da
jaycorpstudios <jaycorpstudios@me.com>
2020-03-14 14:12:37 -0600

    MED-3-upgrade-storybook-addons: Fixed font issues

--- packages/lib-sheika-components/.storybook/preview.js
+++ packages/lib-sheika-components/.storybook/preview.js
@@ -1,10 +1,12 @@
 import { addParameters } from '@storybook/react';
 import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import centered from '@storybook/addon-centered/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import './globals.scss';
 
 addDecorator(withInfo); 
+addDecorator(centered);
 
 addParameters({
     viewport: {
