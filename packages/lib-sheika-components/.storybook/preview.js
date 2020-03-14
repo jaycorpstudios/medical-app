f39465f3abec9ddf02f9af2f893131eb8f589ca2
jaycorpstudios <jaycorpstudios@me.com>
2020-03-14 13:59:10 -0600

    MED-3-upgrade-storybook-addons: Updated stories with info

--- packages/lib-sheika-components/.storybook/preview.js
+++ packages/lib-sheika-components/.storybook/preview.js
@@ -1,1 +1,14 @@
+import { addParameters } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import './globals.scss';
+
+addDecorator(withInfo); 
+
+addParameters({
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'responsive',
+    },
+  });
